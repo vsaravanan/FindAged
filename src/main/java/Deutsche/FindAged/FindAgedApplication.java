93b54d45ad560ef1890c38e68765842d8fab164e
Viswanathan Saravanan <saravanan.resume@gmail.com>
2025-09-30 14:53:16 +0800

    second-

---
+++ src/main/java/Deutsche/FindAged/FindAgedApplication.java
@@ -0,0 +1,13 @@
+package Deutsche.FindAged;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindAgedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindAgedApplication.class, args);
+	}
+
+}
